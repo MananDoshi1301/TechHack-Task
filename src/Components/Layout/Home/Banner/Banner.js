b57d736ebd32c154b3a7bfe411f1e3c913be52a7
Manan Doshi <manandoshi1301@gmail.com>
2021-06-04 18:33:07 +0530

    Banner img complete

--- src/Components/Layout/Home/Banner/Banner.js
+++ src/Components/Layout/Home/Banner/Banner.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import bannerImg from '../../../Images/newBackCopy.png';
+import bannerImg from '../../../Images/pic.jpg';
 import './Banner.css'
 import styles from './banner.module.css';
 
 const Banner = () => {
     return (
         <div className='container-fluid p-0'>
-            <div className={`banner`}
-                style={{
-                    backgroundSize:'cover',
-                    backgroundImage: `url(${bannerImg})`,
-                    backgroundPosition: 'center center',
-                    width:'100%',
-                    height:'600px'
-                }}
-            />            
+            <div className="banner row">
+                <div className="col-lg-6"></div>
+                <div className="col-lg-6">
+                    <img src={bannerImg} alt="" className='img-fluid'/>
+                </div>
+            </div>                        
         </div>
     )
 }
