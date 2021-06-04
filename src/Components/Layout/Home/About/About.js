d97d9ab9c18bdbe3b0cc9bc2b1749d5eb4195230
Manan Doshi <manandoshi1301@gmail.com>
2021-06-05 01:12:17 +0530

    About Section Complete

---
+++ src/Components/Layout/Home/About/About.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './about.module.css';
+import whyUsImg from '../../../Images/whyUs.jpg';
+
+const About = () => {
+  return (
+    <div>
+      <div className='container my-5'>
+        <div className={`display-1 text-center ${styles.aboutHead}`}>
+          Start Your Career With THT!
+        </div>
+        <div className={`row`}>
+          <div
+            className={`col-lg-12 ${styles.aboutText} fs-2 text-center mt-3`}
+          >
+            <span className=''>TechHack</span> is the only training firm in
+            India to provide regular live training sessions, assured internships
+            and over 10 industry projects. We have trained over 800 students in
+            the field of cyber security and our students have been recognised by
+            the government of India. Our major objective is to create a one stop
+            solution for learning Cyber security from scratch. We offer courses
+            right from extreme beginner level up-to super advanced. Each
+            training program is packed with tons of Internship and freelancing
+            opportunities.
+          </div>
+        </div>
+      </div>
+      <div className='container my-5'>
+        <div className={`display-1 text-center ${styles.aboutHead} my-3`}>
+          Why Us?
+        </div>
+        <div className='row'>
+          <div className={`col-lg-6`}>
+            <img className={`img-fluid`} src={whyUsImg} alt='' />
+          </div>
+          <div className={`col-lg-6 fs-3`}>
+            TechHack provides Two assured Internship opportunities and over 10
+            industry projects simultaneously with the regular live training. We
+            have zero pre-recorded sessions and committed to the practical based
+            approach. Throughout the course, you will involve in numerous real
+            time projects of real start-ups and get global standard exposure. We
+            will handhold you at each and every step with our 24/7 expert
+            support. Weekly practical and assignments will be provided for
+            better sanctity. Our mentors have an average experience of 3+ years
+            and they have worked with top global firms such as Requestee,
+            Lazyhackers and many more.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Components/Layout/Home/Home.js
+++ src/Components/Layout/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from '../Common/Nav/Nav';
+import About from './About/About';
 import Banner from './Banner/Banner'
 
 const Home = () => {
@@ -7,7 +8,7 @@ const Home = () => {
         <div>
             <Nav />
             <Banner />  
-            
+            <About />
         </div>
     )
 }
