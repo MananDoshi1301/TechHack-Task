3dd5d637ce66f6ca291b556f455f4ca4b1653eb3
Manan Doshi <manandoshi1301@gmail.com>
2021-06-05 01:44:46 +0530

    Webinar page created

---
+++ src/Components/Layout/Home/Webinar/Webinar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './webinar.module.css';
+
+const Webinar = () => {
+    return (
+        <div>
+            <div className="container">
+               <div></div>     
+            </div>
+        </div>
+    )
+}
+
+export default Webinar
